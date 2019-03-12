ff54ec96625952f6fa3ba0fa1129d4dbf456c571
Jonah Witt <jonahwitt@Jonahs-MacBook-Pro.local>
2019-03-12 19:57:18 -0400

    Began data cleaning

--- FinalProject.R
+++ FinalProject.R
@@ -6,6 +6,7 @@
 ########################################################################
 
 # Load Libraries
+library(stringr)
 library(e1071)
 library(randomForest)
 library(caret)
@@ -16,17 +17,55 @@ library(ggplot2)
 library(arules)
 
 # Set working directory
-setwd("~/Desktop/IST 707/Project")
+setwd("~/Desktop/IST 707/IST707FinalProject")
 
 # Load in the data
-df <- read.csv("WorldCups.csv")
+worldCupData <- read.csv("WorldCups.csv")
+matchData <- read.csv("WorldCupMatches.csv")
 
 
 ########################################################################
 ## Data Cleaning
 ########################################################################
 
-# Remove Periods
+# Remove ID variables from match data
+matchData <- matchData[ , -which(names(matchData) %in% c("RoundID","MatchID"))]
+
+# Remove Team Initials from match data
+matchData <- matchData[ , -which(names(matchData) %in% c("Home.Team.Initials","Away.Team.Initials"))]
+
+# Remove Date and only keep time in Datetime
+matchData$Datetime <- str_extract(matchData$Datetime, '[0-9][0-9]:[0-9][0-9]')
+
+# Change column names in match data
+colnames(matchData) <- c("year", "time", "stage", "stadium", "city", "homeTeam", "hometeamGoals", "awayTeamGoals", 
+                         "awayTeam", "winConditions", "attendance", "halfTimeHomeGoals", "halfTimeAwayGoals", 
+                         "referee", "assistant1", "assistant2")
+
+# Check for NA in match data
+summary(complete.cases(matchData))
+
+# Remove Win Conditions Column from match data. Too many missing values.
+matchData <- matchData[ , -which(names(matchData) %in% c("winConditions"))]
+
+# Remove remaining records with missing values in match data
+matchData <- matchData[complete.cases(matchData),]
+
+# Get nationality of officials
+i <- 1
+for(row in matchData){
+  matchData$refereeNationality[i] <- gsub("[\\(\\)]", "", regmatches(matchData$referee[i], 
+                                                                  gregexpr("\\(.*?\\)", 
+                                                                  matchData$referee[i]))[[1]])
+  matchData$assistant1Nationality[i] <- gsub("[\\(\\)]", "", regmatches(matchData$assistant1[i], 
+                                                                     gregexpr("\\(.*?\\)", 
+                                                                              matchData$assistant1[i]))[[1]])
+  matchData$assistant2Nationality[i] <- gsub("[\\(\\)]", "", regmatches(matchData$assistant2[i], 
+                                                                     gregexpr("\\(.*?\\)", 
+                                                                              matchData$assistant2[i]))[[1]])
+  i <- i + 1
+}
+
 
 ########################################################################
 ## Data Visualization
